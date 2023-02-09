06859c57957808c295b1305cafe0401b4e192f5d
Lupesms97 <lupesms97@gmail.com>
2023-02-09 16:40:04 -0300

    1 primeiro commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// var task 
+var task_Titulo;
+
+// adiconar task botão
+document.getElementById("add-tasks").onclick = function(){
+    task_Titulo = document.getElementById("task-titulo").value; 
+
+}
+
+
+
+
