a30d90c5b2e7dbd72414dcd9707c234ba91c71c0
Lupesms97 <lupesms97@gmail.com>
2023-02-10 16:11:19 -0300

    Implementação do botão add task

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 
-// var task 
 
 
 // let list = JSON.parse(localStorage.getItem("tasks")) || []
-var lista = [];
 
-function adiconar_Task(a){
-    lista.unshift(a);
-    return console.log(lista.toString());
-    
-}
+
 
 
 
 // adiconar task - botão
 document.getElementById("add-tasks").onclick = function(){
+
+    // variavel local com a lista
+    let lista;
+
+    // Recupera a string JSON da lista do Local Storage
+    let listaJSON = localStorage.getItem('lista');
+
+    // Converte a string JSON de volta para uma lista de JavaScript
+    lista = JSON.parse(listaJSON);
 
     // titulo
     var task_Titulo =  document.getElementById("task-titulo").value;
@@ -36,14 +39,51 @@ document.getElementById("add-tasks").onclick = function(){
     // console.log(valor_selecionado);
 
     var task = {
-        'titulo': task_Titulo,
-        'descricao': task_descricao,
-        'data': task_data,
-        'prioridade' : valor_selecionado
+        "titulo": task_Titulo,
+        "descricao": task_descricao,
+        "data": task_data,
+        "prioridade": valor_selecionado
+    };
+    
+    lista.push(task);
+
+
+    // console.log(lista);
+    // console.log(task);
+    // console.log(task.titulo);
+    // console.log(task.descricao);
+
+    // traansformando lista em json
+    listaJSON = JSON.stringify(lista);
+
+    try {    
+        // salvando lista em LocalStorage
+        localStorage.setItem('lista', listaJSON);
+        document.getElementById("task-titulo").value = "";
+        document.getElementById("task-descricao").value = "";
+        document.getElementById("prazo").value = "";
+        campo_select.options.selectedIndex.value = "opt1";
+        
+    } catch (error) {
+
+        console.error(error);
+        
+    }finally{
+        // task_data = "";
+        // valor_selecionado = "";
+        // task_descricao = "";
+        // task_Titulo = "";
 
     }
+    // document.getElementById("spinner-loader").style.display = "initial";
+    // window.setTimeout(function(){
+    //     document.getElementById("spinner-loader").style.display = "none";
+    // }, 2000);
 
-    adiconar_Task(task);
+
+
+
+
 
   
 
@@ -53,18 +93,6 @@ document.getElementById("add-tasks").onclick = function(){
 };
 
 // var lista = document.getElementById("tasks-list").value;
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // var task = (function() {
